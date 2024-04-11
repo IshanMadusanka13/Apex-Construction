56120e030734dcc6e88e35c0814db7773f7223ed
Ishan <madusankaishan13@gmail.com>
2024-04-11 11:32:49 +0530

    incomplete2

--- client/src/pages/PackageManagement/ViewPackage.js
+++ client/src/pages/PackageManagement/ViewPackage.js
@@ -60,68 +60,118 @@ function ViewPackage() {
         }
     }
 
+    //--------------------Table Functions---------------------------------
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const StyledTableCell = styled(TableCell)(() => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.text.default,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+
+    const StyledTableRow = styled(TableRow)(() => ({
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.primary.mainOpacity,
+        },
+        '&:nth-of-type(even)': {
+            backgroundColor: theme.palette.primary.mainOpacity2,
+        },
+        // hide last border
+        '&:last-child td, &:last-child th': {
+            border: 0,
+        },
+    }));
+
+    //-------------------------------Table Functions-----------------------------
+
     return (
         <Grid container>
-                <Grid
-                    item md={12}
-                    spacing={2}
-                    component="form"
-                    sx={theme.palette.gridBody}
-                    noValidate
-                    onSubmit={handleSubmit}
-                >
-                    <Grid item md={12}>
-                        <Typography variant="h5" gutterBottom>
-                            View Package
-                        </Typography>
-                    </Grid>
-                    <Button type="submit" variant="contained" sx={{ mt: 3, width: "50%" }}>
-                        Search Package
-                    </Button>
+            <Grid
+                item md={12}
+                spacing={2}
+                component="form"
+                sx={theme.palette.gridBody}
+                noValidate
+                onSubmit={handleSubmit}
+            >
+                <Grid item md={12}>
+                    <Typography variant="h5" gutterBottom>
+                        View Package
+                    </Typography>
                 </Grid>
+                <Button type="submit" variant="contained" sx={{ mt: 3, width: "50%" }}>
+                    Search Package
+                </Button>
+            </Grid>
             <Grid item md={15} sx={theme.palette.gridBody} textAlign="center">
                 <Typography variant="h4" gutterBottom>
                     All Packages
                 </Typography>
+
+
+
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 1000 }}>
                         <TableHead>
-                            <TableRow>
+                            <StyledTableRow>
                                 {/* <TableCell>Package ID</TableCell> */}
-                                <TableCell>PackageId</TableCell>
-                                <TableCell>Name</TableCell>
-                                <TableCell>Price</TableCell>
-                                <TableCell>Description</TableCell>
-                                <TableCell>Duration</TableCell>
-                                {/* <TableCell>Home Image</TableCell>
-                        <TableCell>Model Link</TableCell> */}
-                                <TableCell>Cost</TableCell>
-                                <TableCell>Action</TableCell>
-                            </TableRow>
+                                <StyledTableCell>PackageId</StyledTableCell>
+                                <StyledTableCell>Name</StyledTableCell>
+                                <StyledTableCell>Price</StyledTableCell>
+                                <StyledTableCell>Description</StyledTableCell>
+                                <StyledTableCell>Duration</StyledTableCell>
+                                {/* <StyledTableCell>Home Image</StyledTableCell>
+                        <StyledTableCell>Model Link</StyledTableCell> */}
+                                <StyledTableCell>Cost</StyledTableCell>
+                                <StyledTableCell>Action</StyledTableCell>
+                            </StyledTableRow>
                         </TableHead>
                         <TableBody>
                             {packageDetails.map((row, Index) => (
-                                <TableRow key={row.packageId}>
-                                    <TableCell>{row._id}</TableCell>
-                                    <TableCell>{row.name}</TableCell>
-                                    <TableCell>{row.price}</TableCell>
-                                    <TableCell>{row.description}</TableCell>
-                                    <TableCell>{row.duration}</TableCell>
-                                    {/* <TableCell>{row.homeImage}</TableCell>
-                            <TableCell>{row.modelLink}</TableCell> */}
-                                    <TableCell>{row.cost}</TableCell>
-                                    <TableCell>
+                                <StyledTableRow key={row.packageId}>
+                                    <StyledTableCell>{row._id}</StyledTableCell>
+                                    <StyledTableCell>{row.name}</StyledTableCell>
+                                    <StyledTableCell>{row.price}</StyledTableCell>
+                                    <StyledTableCell>{row.description}</StyledTableCell>
+                                    <StyledTableCell>{row.duration}</StyledTableCell>
+                                    {/* <StyledTableCell>{row.homeImage}</StyledTableCell>
+                            <StyledTableCell>{row.modelLink}</StyledTableCell> */}
+                                    <StyledTableCell>{row.cost}</StyledTableCell>
+                                    <StyledTableCell>
                                         <IconButton onClick={() => handleDelete(row._id)}>
                                             <Delete />
                                         </IconButton>
                                         <IconButton onClick={() => HandleUpdate(row._id)}>
                                             <Edit />
                                         </IconButton>
-                                    </TableCell>
-                                </TableRow>
+                                    </StyledTableCell>
+                                </StyledTableRow>
                             ))}
                         </TableBody>
                     </Table>
+                    <TablePagination
+                        sx={{ backgroundColor: theme.palette.primary.main, }}
+                        rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                        count={rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
                 </TableContainer>
             </Grid>
         </Grid>
