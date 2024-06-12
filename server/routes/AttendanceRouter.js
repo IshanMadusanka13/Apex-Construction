7a501b60f744ea9c3317a73efd28ba989cf0fff1
Ishan <madusankaishan13@gmail.com>
2024-06-12 20:38:17 +0530

    removal

--- server/routes/AttendanceRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllAttendanceRecords, markAttendance } from '../controller/attendanceController.js';
-
-const attendanceRouter = express.Router();
-
-attendanceRouter.get('/getid/:id', getAllAttendanceRecords);
-attendanceRouter.post('/mark', markAttendance);
-
-export default attendanceRouter;
--- server/routes/EmployeeSalaryRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { assignSalary } from '../controller/employeeSalaryController.js';
-
-
-const employeeSalaryRouter = express.Router();
-
-employeeSalaryRouter.post('/assign-salary', assignSalary);
-
-export default employeeSalaryRouter;
